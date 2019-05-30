565a504b04192b4b50a51963e4eeb5c060dd2863
Gunter Königsmann <gunter@peterpall.de>
2019-05-30 17:19:33 +0200

    That is what a ci is for: Telling us if we forget to commit files.

---
+++ src/VariablesPane.cpp
@@ -0,0 +1,44 @@
+// -*- mode: c++; c-file-style: "linux"; c-basic-offset: 2; indent-tabs-mode: nil -*-
+//
+//  Copyright (C) 2018 Gunter Königsmann <wxMaxima@physikbuch.de>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  SPDX-License-Identifier: GPL-2.0+
+
+#include "VariablesPane.h"
+
+Variablespane::Variablespane(wxWindow *parent, wxWindowID id) : wxGrid(parent, id)
+{
+  SetMinSize(wxSize(wxSystemSettings::GetMetric ( wxSYS_SCREEN_X )/10,
+                    wxSystemSettings::GetMetric ( wxSYS_SCREEN_Y )/10));
+  CreateGrid(1,2);
+  wxGridCellAttr *attr0, *attr1;
+  attr0 = new wxGridCellAttr;
+  attr0->SetRenderer(new wxGridCellAutoWrapStringRenderer);
+  SetColAttr(0,attr0);
+  SetColLabelValue(0,_("Variable"));
+  attr1 = new wxGridCellAttr;
+  attr1->SetReadOnly();
+  attr1->SetRenderer(new wxGridCellAutoWrapStringRenderer);
+  SetColAttr(1,attr1);
+  SetColLabelValue(1,_("Contents"));
+  HideRowLabels();
+}
+
+Variablespane::~Variablespane()
+{
+}
